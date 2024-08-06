230ae83156e66005823adf45f99ef14655117c9e
Gustavo Rodrigues <gustavorodri.dev@gmail.com>
2024-08-05 21:50:16 -0300

    firt tarefa

---
+++ src/list/operacoesbasicas/Tarefa.java
@@ -0,0 +1,15 @@
+package list.operacoesbasicas;
+
+public class Tarefa {
+    // atributo
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+}
