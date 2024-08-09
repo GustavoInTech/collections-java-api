db90bd92b95caa32e0965f18d5642c2ab35f0f41
Gustavo Rodrigues <gustavorodri.dev@gmail.com>
2024-08-09 20:26:40 -0300

    criado os metods da clas pessoa

--- src/list/ordenacao/Pessoa.java
+++ src/list/ordenacao/Pessoa.java
@@ -1,5 +1,36 @@
 package list.ordenacao;
 
 public class Pessoa {
+    // atributos
+    private String nome;
+    private int idade;
+    private double altura;
+
+    public Pessoa(String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", altura=" + altura +
+                '}';
+    }
 
 }
